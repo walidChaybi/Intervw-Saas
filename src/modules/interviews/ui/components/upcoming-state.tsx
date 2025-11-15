b3f819144ae38738f0c01ca9f61efc4d939a750d
walidchaybi <ingchaybi@gmail.com>
2025-11-15 20:22:05 +0100

    Refactor interview components to replace 'meeting' terminology with 'interview'; update props to use 'interviewId' for consistency; enhance processing and upcoming states for better user experience in interview management.

--- src/modules/interviews/ui/components/upcoming-state.tsx
+++ src/modules/interviews/ui/components/upcoming-state.tsx
@@ -5,22 +5,22 @@ import { Button } from "@/components/ui/button";
 import { EmptyState } from "@/components/empty-state";
 
 interface Props {
-  meetingId: string;
+  interviewId: string;
 }
 
-export const UpcomingState = ({ meetingId }: Props) => {
+export const UpcomingState = ({ interviewId }: Props) => {
   return (
     <div className="bg-white rounded-lg px-4 py-5 flex flex-col gap-y-8 items-center justify-center">
       <EmptyState
         image="/upcoming.svg"
         title="Not started yet"
-        description="Once you start this meeting, a summary will appear here"
+        description="Once you start this interview, a summary will appear here"
       />
       <div className="flex flex-col-reverse lg:flex-row lg:justify-center items-center gap-2 w-full">
         <Button asChild className="w-full lg:w-auto">
-          <Link href={`/call/${meetingId}`}>
+          <Link href={`/interviews/${interviewId}`}>
             <VideoIcon />
-            Start meeting
+            Start interview
           </Link>
         </Button>
       </div>
