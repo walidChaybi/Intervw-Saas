a92141af917957ed41fc9bc1fcf09d9447131a28
walidChaybi <98161047+walidChaybi@users.noreply.github.com>
2025-12-15 11:00:37 +0100

    Create MiseAJourRequete.tsx

---
+++ MiseAJourRequete.tsx
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { EditionMiseAJourContext } from "../../../contexts/EditionMiseAJourContextProvider";
+import ExtractionFeedback from "../../commun/feedback/ExtractionFeedback";
+import PartieActe from "./PartieActe";
+import PartieFormulaire from "./PartieFormulaire";
+
+const MiseAJourRequete: React.FC = () => {
+  const { estActeSigne } = useContext(EditionMiseAJourContext.Valeurs);
+
+  return (
+    <>
+      <div className="mt-3 flex gap-4 px-2">
+        <PartieActe />
+        {!estActeSigne && <PartieFormulaire />}
+      </div>
+      <ExtractionFeedback />
+    </>
+  );
+};
+
+export default MiseAJourRequete;
