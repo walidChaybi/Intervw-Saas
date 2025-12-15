2eeec1f4a9c412258214d2ef6820845df45542e8
walidChaybi <98161047+walidChaybi@users.noreply.github.com>
2025-12-15 11:01:02 +0100

    Create PageEditionRequeteMiseAJour.tsx

---
+++ PageEditionRequeteMiseAJour.tsx
@@ -0,0 +1,33 @@
+import { TUuidActeParams } from "@model/params/TUuidActeParams";
+import { Navigate, useParams } from "react-router";
+import MiseAJourRequete from "../../composants/pages/requetesMiseAJour/MiseAJourRequete";
+import EditionMiseAJourContextProvider from "../../contexts/EditionMiseAJourContextProvider";
+import ExtractionTexteContextProvider from "../../contexts/ExtractionTexteContextProvider";
+import { URL_ACCUEIL } from "../../router/infoPages/InfoPagesBase";
+
+interface IPageEditionRequeteMiseAJourProps {
+  estMiseAJourAvecMentions?: boolean;
+}
+
+const PageEditionRequeteMiseAJour: React.FC<IPageEditionRequeteMiseAJourProps> = ({ estMiseAJourAvecMentions = false }) => {
+  const { idActeParam, idRequeteParam } = useParams<TUuidActeParams>();
+
+  return !idActeParam || !idRequeteParam ? (
+    <Navigate
+      to={URL_ACCUEIL}
+      replace
+    />
+  ) : (
+    <EditionMiseAJourContextProvider
+      idActe={idActeParam}
+      idRequete={idRequeteParam}
+      estMiseAJourAvecMentions={estMiseAJourAvecMentions}
+    >
+      <ExtractionTexteContextProvider>
+        <MiseAJourRequete />
+      </ExtractionTexteContextProvider>
+    </EditionMiseAJourContextProvider>
+  );
+};
+
+export default PageEditionRequeteMiseAJour;
